b2e72b00ab6e67108d9c86c613cff9ed09c248bb
Emily <emily.buch@mail.wlc.edu>
2023-04-16 16:31:30 -0500

    actors can move in move.cpp and move.h

--- content/actions/move.cpp
+++ content/actions/move.cpp
@@ -1,5 +1,14 @@
 #include "move.h"
 
+#include <iostream>
+
+#include "actor.h"
+
+Move::Move(Vec direction) : direction{direction} {}
+
 Result Move::perform(Engine&) {
+    Vec position = actor->get_position();
+    actor->move_to(position + direction);
+
     return success();
 }
--- content/actions/move.h
+++ content/actions/move.h
@@ -1,8 +1,13 @@
 #pragma once
 
 #include "action.h"
+#include "vec.h"
 
 class Move : public Action {
 public:
+    Move(Vec direction);
     Result perform(Engine& engine) override;
+
+private:
+    Vec direction;
 };
