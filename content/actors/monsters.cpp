a836db06ba7a537c09ed3fd8e39de926094ccbe3
Emily <emily.buch@mail.wlc.edu>
2023-04-26 13:26:04 -0500

    added three monsters

--- content/actors/monsters.cpp
+++ content/actors/monsters.cpp
@@ -1,1 +1,32 @@
 #include "monsters.h"
+
+#include "none.h"
+#include "rest.h"
+
+namespace Monsters {
+
+std::unique_ptr<Action> default_behavior(Engine& engine, Monster& m) {
+    return std::make_unique<Rest>();
+}
+
+constexpr int default_speed{8};
+
+MonsterType goblin() {
+    int health = 2;
+    return {"goblin", default_speed, health, std::make_shared<None>(),
+            default_behavior};
+}
+
+MonsterType skeleton() {
+    int health = 2;
+    return {"skeleton", default_speed, health, std::make_shared<None>(),
+            default_behavior};
+}
+
+MonsterType demon_tiny() {
+    int health = 2;
+    return {"demon_tiny", default_speed, health, std::make_shared<None>(),
+            default_behavior};
+}
+
+}  // namespace Monsters
--- content/actors/monsters.h
+++ content/actors/monsters.h
@@ -1,7 +1,14 @@
 #pragma once
 
+#include "action.h"
+#include "monstertype.h"
+
 namespace Monsters {
-    
 
+std::unique_ptr<Action> default_behavior(Engine& engine, Monster& m);
 
-}
+MonsterType goblin();
+MonsterType skeleton();
+MonsterType demon_tiny();
+
+}  // namespace Monsters
