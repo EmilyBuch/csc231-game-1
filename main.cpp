9d084eadabe44aed4b558fdd8b510ebdac9b65a2
Emily <emily.buch@mail.wlc.edu>
2023-04-26 13:29:11 -0500

    main will spawn 50 monsters of each type

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include "engine.h"
 #include "heros.h"
+#include "monsters.h"
 
 int main() {
     std::string filename{"settings.txt"};
     Settings settings = read_settings(filename);
     Engine engine{settings};
+
+    for (int i = 0; i < 50; ++i) {
+        engine.create_monster(Monsters::goblin());
+    }
+    for (int i = 0; i < 50; ++i) {
+        engine.create_monster(Monsters::demon_tiny());
+    }
+    for (int i = 0; i < 50; ++i) {
+        engine.create_monster(Monsters::skeleton());
+    }
 
     engine.create_hero(Heros::nobody);
     engine.run();
