39707c85c57cbf5f0b50a377562ab9edd9c01a22
Emily <emily.buch@mail.wlc.edu>
2023-05-10 09:11:46 -0500

    fixed swing

--- content/events/swing.cpp
+++ content/events/swing.cpp
@@ -12,7 +12,8 @@ Swing::Swing(Sprite& sprite, Vec direction, Actor& defender, int damage)
       sprite{sprite},
       original{sprite},
       defender{defender},
-      damage{damage} {
+      damage{damage},
+      starting_angle{sprite.angle} {
     if (direction == Vec{1, 0}) {  // right
         starting_angle = 5;
         delta_angle = 130.0 / (duration - 1);
@@ -26,11 +27,11 @@ Swing::Swing(Sprite& sprite, Vec direction, Actor& defender, int damage)
         starting_angle = 135 * sign;
         delta_angle = 130.0 / (duration - 1) * sign;
     } else {  // up
+        sprite.shift.y -= 12;
         double sign = std::copysign(1.0, starting_angle);
         // 15 -> 1 and -15 -> -1
         starting_angle = -75 * sign;
         delta_angle = 130.0 / (duration - 1) * sign;
-        sprite.shift.y -= 12;
     }
 }
 
